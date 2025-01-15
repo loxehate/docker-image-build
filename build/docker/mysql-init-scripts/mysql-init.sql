1320621283870edd409c8a058f39d1be8a4bdc06
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-01-15 16:35:24 +0800

    update

--- build/docker/mysql-init-scripts/mysql-init.sql
+++
@@ -1,9 +0,0 @@
--- 创建数据库
-CREATE DATABASE IF NOT EXISTS superset;
-
--- 创建用户并授予权限
-CREATE USER IF NOT EXISTS 'superset'@'%' IDENTIFIED BY 'superset';
-GRANT ALL PRIVILEGES ON superset.* TO 'superset'@'%';
-
--- 刷新权限
-FLUSH PRIVILEGES;
