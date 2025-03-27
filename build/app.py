958a86601061a1ffd4d3b5272c36e6b007a069ae
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-03-27 15:53:53 +0800

    update

--- build/app.py
+++ build/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    env = os.getenv("APP_ENV", "dev")  # 默认为 dev
+    if env == "prod":
+        return "Hello prod"
+    else:
+        return "Hello dev"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
