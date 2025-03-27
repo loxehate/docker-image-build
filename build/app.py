3dfc00acca5a4c3190af5837ffb54b10c5c0041b
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-03-27 15:14:53 +0800

    update

---
+++ build/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+app.run(debug=True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, my first docker images! ' + os.getenv("HOSTNAME") + ''
